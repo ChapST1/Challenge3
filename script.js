6b53b31f2cc4685239c3b10c7be4af5769b3c6fa
ChaPst1 <3lprxd@gmail.com>
2021-12-20 23:32:13 -0500

    nj

--- script.js
+++ script.js
@@ -2,4 +2,23 @@ let menu = document.getElementById("btn");
 menu.addEventListener('click', ()=>{
   let bars = document.getElementById("barras");
   bars.classList.toggle("active");
+});
+//info
+let mas = document.getElementById("more");
+mas.addEventListener('click',()=>{
+  let infor = document.getElementById("info");
+  infor.classList.toggle("descripcionActive");
 })
+
+//votos
+let corazon = document.getElementById("heart");
+corazon.addEventListener('click',()=>{
+  corazon.classList.toggle("fa-heart-active");
+})
+
+let corazonRoto = document.getElementById("heartBroken");
+corazonRoto.addEventListener('click',()=>{
+  corazonRoto.classList.toggle("fa-heart-broken-active");
+})
+
+
